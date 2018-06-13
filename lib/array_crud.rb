507ce7a72296ed7c67f39d92ac39b1f1d00b9f24
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-13 02:33:56 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,23 +3,23 @@ def create_an_empty_array
 end
 
 def create_an_array
- create_an_array = ["Lisa", "Theresa", "Danny", "Melanie"]
+ 
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("arrays!")
+
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("wow")
+
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift
+
 
 end
 
